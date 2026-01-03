22b26a076a65382bc728e49efe0960aafb47fc46
Youssef Mohamed <yassooff2003@gmail.com>
2026-01-03 17:23:41 +0200

    refactor: Update user answers retrieval logic and improve code formatting

--- js/result_script.js
+++ js/result_script.js
@@ -1,4 +1,4 @@
- const page = location.pathname;
+const page = location.pathname;
 
 if (page.includes("exam.html") || page.includes("result.html")) {
   history.pushState(null, "", location.href);
@@ -7,8 +7,9 @@ if (page.includes("exam.html") || page.includes("result.html")) {
     history.pushState(null, "", location.href);
   };
 }
-const storedAnswers = localStorage.getItem('exam_user_answers');
-const userAnswers = storedAnswers ? JSON.parse(storedAnswers) : new Array(questions.length).fill(null);
+const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+const storedAnswers = currentUser ? currentUser.exam_answers : null;
+const userAnswers = storedAnswers ? storedAnswers : new Array(questions.length).fill(null);
 
 const total = questions.length;
 let correct = 0;
@@ -20,22 +21,22 @@ questions.forEach((q, i) => {
 const wrong = total - correct;
 const percent = Math.round((correct / total) * 100);
 
-let grade = "F" , msg ="Hard luck";
+let grade = "F", msg = "Hard luck";
 if (percent >= 85) grade = "A";
 else if (percent >= 75) grade = "B";
 else if (percent >= 60) grade = "C";
 else if (percent >= 50) grade = "D";
 
-if(percent >= 50)
-  { msg="Congratulations";
-    good_sound.play();
-  }
+if (percent >= 50) {
+  msg = "Congratulations";
+  good_sound.play();
+}
 
 document.getElementById("summary").innerHTML =
   SummaryComponent(total, correct, wrong, grade);
 
 document.getElementById("progress").innerHTML =
-  ProgressCircle(percent , grade);
+  ProgressCircle(percent, grade);
 
 const currentUserJSON = localStorage.getItem("currentUser");
 
